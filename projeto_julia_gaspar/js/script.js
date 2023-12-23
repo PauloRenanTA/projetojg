e3be5b71aac5a1ebd94e3a327724dafc0964007c
fabricio <fabricioffa08@gmail.com>
2023-12-23 17:48:53 -0300

    comments

--- projeto_julia_gaspar/js/script.js
+++ projeto_julia_gaspar/js/script.js
@@ -1,132 +1,126 @@
 // Alternar a visibilidade do menu hambúrguer na versão mobile
 function clickMenu() {
-    let menuMobile = document.getElementById('menu-mobile');
-        if (window.getComputedStyle(menuMobile).display === 'flex') {
-        //Se o menu estiver visível, oculte-o
-        menuMobile.style.display = 'none';
-        } else {
-        //Se o menu estiver oculto, exiba-o
-        menuMobile.style.display = 'flex';
-        }
-    }
+	let menuMobile = document.getElementById('menu-mobile');
+	if (window.getComputedStyle(menuMobile).display === 'flex') {
+		//Se o menu estiver visível, oculte-o
+		menuMobile.style.display = 'none';
+	} else {
+		//Se o menu estiver oculto, exiba-o
+		menuMobile.style.display = 'flex';
+	}
+}
 
-
-/*Interações com o menu inicial na versão para telas maiores. Adição de rolagem automática até uma sessão específica*/ 
+/*Interações com o menu inicial na versão para telas maiores. Adição de rolagem automática até uma sessão específica*/
 function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-        if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
-
+	const section = document.getElementById(sectionId);
+	if (section) {
+		section.scrollIntoView({ behavior: 'smooth' });
+	}
+}
 
 // Manipuladores de clique no menu
 document.addEventListener('DOMContentLoaded', function () {
-const menuLinks = document.querySelectorAll('#menu a');
-menuLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-        const sectionId = link.getAttribute('href').substring(1);
-        scrollToSection(sectionId);
-    });
+	const menuLinks = document.querySelectorAll('#menu a');
+	menuLinks.forEach(link => {
+		link.addEventListener('click', function (event) {
+			event.preventDefault();
+			const sectionId = link.getAttribute('href').substring(1);
+			scrollToSection(sectionId);
+		});
+	});
 });
-});
-
 
 //Interação dos slides de imagens para versão mobile
 let currentIndex = 0;
 const images = document.querySelectorAll('.container');
 
 function showImage(index) {
-    images.forEach((image, i) => {
-        if (i === index) {
-            image.classList.remove('remova');
-        } else {
-            image.classList.add('remova');
-        }
-    });
+	images.forEach((image, i) => {
+		if (i === index) {
+			image.classList.remove('remova');
+		} else {
+			image.classList.add('remova');
+		}
+	});
 }
 
 //Função para mostrar próxima imagem
 function proxImagem() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    showImage(currentIndex);
+	currentIndex = (currentIndex - 1 + images.length) % images.length;
+	showImage(currentIndex);
 }
 
 //Função para mostrar imagem anterior
 function antImagem() {
-    currentIndex = (currentIndex + 1) % images.length;
-    showImage(currentIndex);
+	currentIndex = (currentIndex + 1) % images.length;
+	showImage(currentIndex);
 }
 
-
 //Interação de slides de imagens para versão telas maiores utilizando Swiper
-let swiper = new Swiper(".slide-content", {
-slidesPerView: 3.5,
-centeredSlides: true,
-spaceBetween: 25,
-pagination: {
-type: "fraction",
-},
-navigation: {
-nextEl: ".swiper-button-next",
-prevEl: ".swiper-button-prev",
-},
+let swiper = new Swiper('.slide-content', {
+	slidesPerView: 3,
+	slidesPerGroup: 1,
+	loop: true,
+	centeredSlides: true,
+	spaceBetween: 25,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
 
 //Programação dos botões dos slides para versão telas maiores
-let nextButton = document.querySelector(".swiper-button-next");
-let prevButton = document.querySelector(".swiper-button-prev");
+let nextButton = document.querySelector('.swiper-button-next');
+let prevButton = document.querySelector('.swiper-button-prev');
 
 let nextClickCount = 0;
 let prevClickCount = 0;
 
-nextButton.addEventListener("click", function () {
-nextClickCount++;
+nextButton.addEventListener('click', function () {
+	nextClickCount++;
 
-    if (nextClickCount % 3 === 0) {
-    swiper.allowSlideNext = false;
-    nextButton.classList.add("swiper-button-disabled");
-    } else {
-        swiper.allowSlideNext = true;
-        nextButton.classList.remove("swiper-button-disabled");
-    }
+	if (nextClickCount % 3 === 0) {
+		swiper.allowSlideNext = false;
+		nextButton.classList.add('swiper-button-disabled');
+	} else {
+		swiper.allowSlideNext = true;
+		nextButton.classList.remove('swiper-button-disabled');
+	}
 });
 
-prevButton.addEventListener("click", function () {
-prevClickCount++;
+prevButton.addEventListener('click', function () {
+	prevClickCount++;
 
-    if (prevClickCount % 3 === 0) {
-    swiper.allowSlideNext = true;
-    nextButton.classList.remove("swiper-button-disabled");
-}
+	if (prevClickCount % 3 === 0) {
+		swiper.allowSlideNext = true;
+		nextButton.classList.remove('swiper-button-disabled');
+	}
 });
-
 
 //Programação dos comentários da sessão feedbacks para a versão mobile
 document.addEventListener('DOMContentLoaded', function () {
-    if (window.innerWidth <= 992) {
-        const comments = document.querySelectorAll('.caixacomentarios');
-        let currentIndex = 0;
+	if (window.innerWidth <= 992) {
+		const comments = document.querySelectorAll('.caixacomentarios');
+		let currentIndex = 0;
 
-        function showComment(index) {
-            comments.forEach((comment, i) => {
-                if (i === index) {
-                    comment.classList.add('visivel');
-                } else {
-                    comment.classList.remove('visivel');
-        }
-    });
-}
+		function showComment(index) {
+			comments.forEach((comment, i) => {
+				if (i === index) {
+					comment.classList.add('visivel');
+				} else {
+					comment.classList.remove('visivel');
+				}
+			});
+		}
 
-function nextComment() {
-    comments[currentIndex].classList.remove('visivel');
-    currentIndex = (currentIndex + 1) % comments.length;
-    comments[currentIndex].classList.add('visivel');
-}
+		function nextComment() {
+			comments[currentIndex].classList.remove('visivel');
+			currentIndex = (currentIndex + 1) % comments.length;
+			comments[currentIndex].classList.add('visivel');
+		}
 
-setInterval(nextComment, 8000);
-} else {
-console.log ("Largura da tela é maior que 992px. Não executando o código Javascript para a Seção de Feedbacks.")
-}
+		setInterval(nextComment, 8000);
+	} else {
+		console.log('Largura da tela é maior que 992px. Não executando o código Javascript para a Seção de Feedbacks.');
+	}
 });
